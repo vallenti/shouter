6c7e53b2286b283ab8c86ccddbbeebbc06520525
Valentin Kolev <vallentin.k@gmail.com>
2017-04-12 14:58:30 +0300

    added application project

---
+++ SimpleMVC.App/AppStart.cs
@@ -0,0 +1,16 @@
+﻿using SimpleHttpServer;
+using SimpleMVC.App.MVC;
+using SimpleMVC.App.Utilities;
+
+namespace SimpleMVC.App
+{
+    class AppStart
+    {
+        static void Main(string[] args)
+        {
+            ShoutExpirationChecker.Run();
+            HttpServer server = new HttpServer(8081, RouteTable.Routes);
+            MvcEngine.Run(server);
+        }
+    }
+}
